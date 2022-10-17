c6847a23aa43ed01bd289395ddd9137d244fabaf
N4SSER <nasserbrown@estudiantec.cr>
2022-10-17 07:20:10 -0600

    clean up

--- gauss.m
+++ gauss.m
@@ -1,12 +1,12 @@
-pkg load symbolic
 warning('off', 'all');
+pkg load symbolic;
 syms x
 %Legendre
 P = @(n) 1/(factorial(n)*2^n)*diff((x^2-1)^n,n);
 %Weights
 W = @(n) 2/((1-x^2)*(diff(P(n))^2));
-%gauss-interval
-gauss = @(f,X,W,a,b) (b-a)/2*double(subs(f,((b-a)/2*X+(b+a)/2)))'*W;
+%Gauss-interval (f(Xn)'W)
+Gauss = @(f,X,W,a,b) (b-a)/2*double(subs(f,((b-a)/2*X+(b+a)/2)))'*W;
 %degree
 n = 5
 %points
@@ -15,6 +15,6 @@ X = double(solve(P(n)));
 Weights  =  double(subs(W(n),X));
 %function
 f = exp(-x)/x
-interval = [1 5]
+interval = [2 5]
 %Result
-result = gauss(f,X,Weights,interval(1),interval(2))
+result = Gauss(f,X,Weights,interval(1),interval(2))
